2f07dccef27a23cc54865fd7f97a5e5deb0f8d5f
Shrashtha Saha <Shrashtha.Saha@cognizant.com>
2026-01-20 12:39:10 +0530

    folders

---
+++ src/main/java/com/example/pages/main.java
@@ -0,0 +1,6 @@
+public class mainnn {
+    public static void main(String[] args) {
+        System.out.println("Welcome to TripAdvisor Automation!");
+        // Initialize and run your automation scripts here
+    }
+}
